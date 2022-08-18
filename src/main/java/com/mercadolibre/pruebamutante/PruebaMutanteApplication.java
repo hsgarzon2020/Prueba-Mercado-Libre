468843ad086fe2cb97b9cb0c85f9b50304cf4e35
henry <henry.garzonc@claro.com.co>
2022-08-17 21:00:38 -0500

    Commit inicial

---
+++ src/main/java/com/mercadolibre/pruebamutante/PruebaMutanteApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.pruebamutante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaMutanteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaMutanteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mercadolibre/pruebamutante/PruebaMutanteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.pruebamutante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaMutanteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
